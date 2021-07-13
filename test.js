659bc96e08f5fb8ddf646c45029ae91644b6e58b
13jun <820373584@qq.com>
2021-07-13 12:21:06 +0800

    优化代码

--- test.js
+++ test.js
@@ -1,43 +1,52 @@
 import string from './exportCss.js'
 let n = 1
-demo2.innerHTML = string.substr(0,n)
+demo2.innerHTML = string.substr(0, n)
 
-const run= ()=>{
+const run = () => {
     n += 1
-    if(n > string.length){
+    if (n > string.length) {
         window.clearInterval(id)
         return
     }
-    demo1.innerText = string.substr(0,n)
-    demo2.innerHTML = string.substr(0,n)
-    demo1.scrollTop =demo1.scrollHeight
-}
-let time = 100
-let id = setInterval(run,time)
-
-btnPause.onclick=()=>{
-    window.clearInterval(id)
-}
-btnPlay.onclick =()=>{
-    id = setInterval(()=>{
-        run()
-    },time)
+    demo1.innerText = string.substr(0, n)
+    demo2.innerHTML = string.substr(0, n)
+    demo1.scrollTop = demo1.scrollHeight
 }
 
-btnSlow.onclick=()=>{
+const play = () => {
+    return setInterval(run, time)
+}
+const clear = ()=>{
     window.clearInterval(id)
+}
+const slow = ()=>{
+    clear()
     time = 300;
-    id = setInterval(()=>{
-        run()
-    },time)
+    id = play()
 }
-btnNormal.onclick=()=>{
-    window.clearInterval(id)
+const normal = ()=>{
+    clear()
     time = 100
-    id = setInterval(run,time)
+    id = play()
 }
-btnFast.onclick=()=>{
-    window.clearInterval(id)
+const fast=()=>{
+    clear()
     time = 0
-    id = setInterval(run,time)
+    id = play()
 }
+
+let time = 100
+let id = play()
+
+btnPause.onclick = () => {
+    clear()
+}
+btnPlay.onclick = () => {
+    id = play()
+}
+
+btnSlow.onclick =slow
+
+btnNormal.onclick = normal
+
+btnFast.onclick = fast
